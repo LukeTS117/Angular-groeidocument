382e855916787dcd9bb23fc26a9a323e2df7e000
Luke <luketaylor001@gmail.com>
2021-03-26 11:16:09 +0100

    Opdracht 1 en gedeeltelijk van 2 tour of heroes

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 2, name: 'Spider-man'},
+    {id: 3, name: 'Wolverine'},
+    {id: 4, name: 'Iron man'},
+    {id: 5, name: 'Captain america'},
+    {id: 6, name: 'Ant man'},
+    {id: 7, name: 'Black panther'},
+    {id: 8, name: 'Hawk eye'},
+    {id: 9, name: 'Thor'},
+    {id: 10, name:'Vision'},
+    {id: 11, name:'Doctor strange'}
+]
