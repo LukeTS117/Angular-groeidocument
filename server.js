ed58870f347a4289d4e022a2116f2884045b74b2
Luke <luketaylor001@gmail.com>
2021-03-30 10:06:36 +0200

    renamed some stuff

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ const app = express();
 
 app.use(requireHTTPS);
 
-app.use(express.static('./dist/<name-on-package.json>'));
+app.use(express.static('dist/vereken-app/styles.d72f14b8a9073c707743.css'));
 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'});
+    res.sendFile('index.html', {root: 'dist/vereken-app/index.html'});
 });
 
 app.listen(process.env.PORT || 8080);
